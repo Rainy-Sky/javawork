422798c298b5debc79e0065a6d50c93a093b0072
Rainy-Sky <54521443+Rainy-Sky@users.noreply.github.com>
2020-06-13 00:39:08 +0800

    初始界面第二次提交（部分未完成

--- src/paperio/PaperIO.java
+++ src/paperio/PaperIO.java
@@ -1,9 +1,16 @@
 package  paperio;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.SourceDataLine;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * PaperIO is the main class used to start window and keep track of current state and switch between states.
@@ -14,9 +21,11 @@ public class PaperIO extends JFrame implements ActionListener{
 
     private Board board;
     private Menu menu;
+    private Record record;
     private Setting setting;
     private JPanel cards;
     private Image im;
+    private static boolean flag=true;
 
     /**
      * 游戏界面初始化
@@ -43,19 +52,28 @@ public class PaperIO extends JFrame implements ActionListener{
 
         menu = new Menu(this);
         setting = new Setting(this);
+        record = new Record(this);
         cards = new JPanel(new CardLayout());
         cards.add(menu, "menu");
         cards.add(setting,"setting");
+        cards.add(record,"record");
         add(cards);
+        
+        new Thread(()->{
+        	while(true) {
+        		playMusic();
+        	}
+        }).start();
     }
 
     /**
-     * 枚举游戏状态：游戏进行（GAME）、菜单（MENU）、设置界面（SETTING）
+     * 枚举游戏状态：游戏进行（GAME）、菜单（MENU）、设置界面（SETTING）、历史记录(record)
      */
     private enum STATE{
         GAME,
         MENU,
-        SETTING
+        SETTING,
+        RECORD
     }
 
     /**
@@ -73,6 +91,10 @@ public class PaperIO extends JFrame implements ActionListener{
             	board.setPaused(true);
         }else if(s==STATE.SETTING) {
         	cardLayout.show(cards, "setting");
+        	if(board!=null)
+        		board.setPaused(true);
+        }else if(s==STATE.RECORD) {
+        	cardLayout.show(cards, "record");
         	if(board!=null)
         		board.setPaused(true);
         }
@@ -106,10 +128,58 @@ public class PaperIO extends JFrame implements ActionListener{
             	setState(STATE.MENU);
             	break;
             case "record":
-            	
+            	setState(STATE.RECORD);
             	break;
         }
     }
+    
+    
+    /*
+     * 设置游戏背景音乐
+     */
+	static void playMusic() {
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new File("E:\\Eclipse\\学生管理系统\\src\\image\\测试2.wav"));
+			AudioFormat aif = ais.getFormat();
+			final SourceDataLine sdl;
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, aif);
+			sdl = (SourceDataLine) AudioSystem.getLine(info);
+			sdl.open(aif);
+			sdl.start();
+			FloatControl fc = (FloatControl) sdl.getControl(FloatControl.Type.MASTER_GAIN);
+			/* 
+			 * value可以用来设置音量，从0-2.0
+			 */
+			double value = 2;
+			float dB = (float) (Math.log(value == 0.0 ? 0.0001 : value) / Math.log(10.0) * 20.0);
+			fc.setValue(dB);
+			int nByte = 0;
+			int writeByte = 0;
+			final int SIZE = 1024 * 64;
+			byte[] buffer = new byte[SIZE];
+			/*
+			 * 判断 播放/暂停 状态
+			 */
+			while (nByte != -1) {
+				if(flag) {
+					nByte = ais.read(buffer, 0, SIZE);
+					sdl.write(buffer, 0, nByte);
+				}else {
+					nByte = ais.read(buffer, 0, 0);
+				}
+			}
+			sdl.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/*
+	 * 设置播放状态
+	 */
+	public static void setMusic(boolean flag){
+		PaperIO.flag=flag;
+	}
+    	
     public static void main(String[] args) {
         System.setProperty("sun.java2d.opengl", "True");
 
--- src/paperio/Record.java
+++ src/paperio/Record.java
@@ -1,13 +1,73 @@
 package paperio;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.TreeSet;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Record extends JPanel {
 	private ActionListener actionListener;
+	private TreeSet<Double> scores;
+	private JLabel achievement;
+	private JLabel[] topscore;
+	private ImageIcon back;
+	private ImageIcon returnM;
+	private JButton button_back;
 	Record(ActionListener actionListener){
 		this.actionListener=actionListener;
+		this.setOpaque(false);
+        back = new ImageIcon("img/achieve.jpg");
+        returnM = new ImageIcon("img/setting_return.jpg");
+        achievement=new JLabel(back);
+		button_back = new JButton(returnM);
+        achievement.setBounds(0, 0, back.getIconWidth(), back.getIconHeight());	
+		topscore=new JLabel[5];
+		this.setLayout(null);
+		init();
+		add(achievement);
+	}
+	/*
+	 * 初始化界面 绘制历史记录内容
+	 */
+	private void init() {
+		int size=0;
+		this.scores=Data.getScores();
+		Iterator<Double> score=scores.iterator();
+		while(score.hasNext()) {
+			topscore[size]=new JLabel("1. "+score.next().toString());
+			size++;
+		}
 		
+		button_back.setBounds(20,7,returnM.getIconWidth(),returnM.getIconHeight());
+        button_back.setActionCommand("menu");
+        button_back.addActionListener(actionListener);
+        button_back.setBackground(Color.DARK_GRAY);
+        button_back.setFocusPainted(false);
+        button_back.setBorderPainted(false);
+        
+        achievement=new JLabel("ACHIEVEMENT");
+        achievement.setFont(new Font("Monospaced", Font.PLAIN, 36));
+        achievement.setForeground(Color.WHITE);
+        achievement.setHorizontalAlignment(JLabel.RIGHT);
+        achievement.setBounds(200, 100, 500, 150);
+		
+        for(int i=0;i<size;i++) {
+        	topscore[i].setFont(new Font("Monospaced", Font.PLAIN, 24));
+        	topscore[i].setForeground(Color.WHITE);
+        	topscore[i].setHorizontalAlignment(JLabel.RIGHT);
+        	topscore[i].setBounds(400, 100+i*100, 300, 100);
+        }
+	}
+	/*
+	 * record更新后 重新绘制界面
+	 */
+	public void refresh() {
+		init();
 	}
 }
