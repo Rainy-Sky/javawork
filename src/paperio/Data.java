5909ab297c5362b661ab7f6cc78651386923b88e
Fewing <yangzheyu98@gmail.com>
2020-06-13 14:00:32 +0800

    data添加name

--- src/paperio/Data.java
+++ src/paperio/Data.java
@@ -1,12 +1,9 @@
-package paperio;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.TreeSet;
-
+import java.util.HashMap;
 
 public class Data {
     private static File record = new File("./", "record.data");
@@ -14,26 +11,27 @@ public class Data {
     private static FileInputStream fileInputStream;
     private static ObjectInputStream objectInputStream;
     private static ObjectOutputStream objectOutputStream;
-    private static TreeSet<Double> scores;
-    
+    private static HashMap<String, Double> scores;
+
     static {
         if (!record.exists()) {
             try {
                 record.createNewFile();
+                fileOutputStream = new FileOutputStream(record);
+                objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(new HashMap<String,Double>());
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public static void init() {
 
-    }
-    public static TreeSet<Double> getScores() {
+    public static HashMap<String, Double> getScores() {
         try {
             fileInputStream = new FileInputStream(record);
             objectInputStream = new ObjectInputStream(fileInputStream);
-            scores =(TreeSet<Double>)objectInputStream.readObject();
+            scores = (HashMap<String, Double>) objectInputStream.readObject();
             return scores;
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,12 +39,14 @@ public class Data {
         }
     }
 
-    public static void addScores(double score) {
+    public static void addScores(String name, double score) {
         try {
             fileInputStream = new FileInputStream(record);
             objectInputStream = new ObjectInputStream(fileInputStream);
-            scores =(TreeSet<Double>)objectInputStream.readObject();
-            scores.add(score);
+            scores = (HashMap<String, Double>) objectInputStream.readObject();
+            if (!scores.containsKey(name) || score > scores.get(name)) {
+                scores.put(name, score);
+            }
             fileOutputStream = new FileOutputStream(record);
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(scores);
@@ -54,11 +54,12 @@ public class Data {
             e.printStackTrace();
         }
     }
+
     public static void clearScores() {
         try {
             fileInputStream = new FileInputStream(record);
             objectInputStream = new ObjectInputStream(fileInputStream);
-            scores =(TreeSet<Double>)objectInputStream.readObject();
+            scores = (HashMap<String, Double>) objectInputStream.readObject();
             scores.clear();
             fileOutputStream = new FileOutputStream(record);
             objectOutputStream = new ObjectOutputStream(fileOutputStream);
